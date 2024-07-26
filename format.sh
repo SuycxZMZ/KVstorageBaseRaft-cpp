81e928cb144c445d96e15dd1017ab5d36394aa10
SuycxZMZ <suycx65@163.com>
2024-07-26 16:39:55 +0800

    添加代码格式化脚本的注释

--- format.sh
+++ format.sh
@@ -1,2 +1,4 @@
 # https://www.cnblogs.com/__tudou__/p/13322854.html
+# 对所有代码重新格式化，这个我改了一下，和原来的项目不一样
+# 本项目不用提交，纯自己学习，缩进注释等格式按个人风格来
 find . -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' ! -regex '.*\(pb\.h\|pb\.cc\)$' -exec clang-format -style=file -i {} \;
