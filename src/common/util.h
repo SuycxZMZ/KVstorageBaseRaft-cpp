dbd82fcf3dffd9d27e9a6b4164cfd86fdceeb90c
Suycx <suycx65@163.com>
2024-08-16 14:37:36 +0800

    修改一下阻塞队列的实现，更高的并发度，代码更优雅

--- src/common/util.h
+++ src/common/util.h
@@ -54,70 +54,75 @@ std::chrono::_V2::system_clock::time_point now();
 std::chrono::milliseconds getRandomizedElectionTimeout();
 void sleepNMilliseconds(int N);
 
-// 异步写日志的日志队列
-// read is blocking!!! LIKE  go chan
 template <typename T>
 class LockQueue {
 public:
-    // 多个worker线程都会写日志queue
+    // 向队列中推送数据（线程安全）
     void Push(const T& data) {
-        std::lock_guard<std::mutex> lock(m_mutex);  // 使用lock_gurad，即RAII的思想保证锁正确释放
-        m_queue.push(data);
-        m_condvariable.notify_one();
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);  
+            m_queue.push(data);
+        }
+        m_condvariable.notify_one();  // 释放锁后通知等待的线程
     }
 
-    // 一个线程读日志queue，写日志文件
+    // 弹出队列中的数据，如果队列为空则阻塞等待（线程安全）
     T Pop() {
         std::unique_lock<std::mutex> lock(m_mutex);
-        while (m_queue.empty()) {
-            // 日志队列为空，线程进入wait状态
-            m_condvariable.wait(lock);  // 这里用unique_lock是因为lock_guard不支持解锁，而unique_lock支持
-        }
-        T data = m_queue.front();
+        m_condvariable.wait(lock, [this]{ return !m_queue.empty(); });  // 阻塞直到队列非空
+        T data = std::move(m_queue.front());  // 使用 move 构造，避免不必要的拷贝
         m_queue.pop();
         return data;
     }
 
-    bool timeOutPop(int timeout, T* ResData)  // 添加一个超时时间参数，默认为 50 毫秒
-    {
+    // 带超时的 Pop 操作，如果超时未获取数据返回 false（线程安全）
+    bool timeOutPop(int timeout, T* ResData) {
         std::unique_lock<std::mutex> lock(m_mutex);
-
-        // 获取当前时间点，并计算出超时时刻
         auto now = std::chrono::system_clock::now();
-        auto timeout_time = now + std::chrono::milliseconds(timeout);
-
-        // 在超时之前，不断检查队列是否为空
-        while (m_queue.empty()) {
-            // 如果已经超时了，就返回一个空对象
-            if (m_condvariable.wait_until(lock, timeout_time) == std::cv_status::timeout) {
-                return false;
-            } else {
-                continue;
-            }
+        
+        // 超时等待队列不为空
+        if (!m_condvariable.wait_until(lock, now + std::chrono::milliseconds(timeout), [this]{ return !m_queue.empty(); })) {
+            return false;  // 超时未获取数据
         }
 
-        T data = m_queue.front();
+        *ResData = std::move(m_queue.front());
         m_queue.pop();
-        *ResData = data;
-        return true;
+        return true;  // 成功获取数据
+    }
+
+    // 获取队列中的第一个元素，但不弹出（线程安全）
+    T front() {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_condvariable.wait(lock, [this]{ return !m_queue.empty(); });  // 阻塞直到队列非空
+        return m_queue.front();  // 返回队列头部的元素
+    }
+
+    // 获取队列的大小（线程安全）
+    size_t size() {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_queue.size();
+    }
+
+    // 检查队列是否为空（线程安全）
+    bool empty() {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_queue.empty();
     }
 
 private:
-    std::queue<T> m_queue;
-    std::mutex m_mutex;
-    std::condition_variable m_condvariable;
+    std::queue<T> m_queue;  // 底层使用 std::queue 存储数据
+    std::mutex m_mutex;  // 保护队列操作的互斥锁
+    std::condition_variable m_condvariable;  // 用于管理阻塞等待的条件变量
 };
-// 两个对锁的管理用到了RAII的思想，防止中途出现问题而导致资源无法释放的问题！！！
-// std::lock_guard 和 std::unique_lock 都是 C++11 中用来管理互斥锁的工具类，它们都封装了 RAII（Resource Acquisition Is
-// Initialization）技术，使得互斥锁在需要时自动加锁，在不需要时自动解锁，从而避免了很多手动加锁和解锁的繁琐操作。
-// std::lock_guard 是一个模板类，它的模板参数是一个互斥量类型。当创建一个 std::lock_guard
-// 对象时，它会自动地对传入的互斥量进行加锁操作，并在该对象被销毁时对互斥量进行自动解锁操作。std::lock_guard
-// 不能手动释放锁，因为其所提供的锁的生命周期与其绑定对象的生命周期一致。 std::unique_lock
-// 也是一个模板类，同样的，其模板参数也是互斥量类型。不同的是，std::unique_lock 提供了更灵活的锁管理功能。可以通过
-// lock()、unlock()、try_lock() 等方法手动控制锁的状态。当然，std::unique_lock 也支持 RAII
-// 技术，即在对象被销毁时会自动解锁。另外， std::unique_lock 还支持超时等待和可中断等待的操作。
 
-// 这个Op是kv传递给raft的command
+/**
+ * @brief 这个Op是kvServer传递给raft的command
+ *            std::string Operation;  // "Get" "Put" "Append"
+              std::string Key;
+              std::string Value;
+              std::string ClientId;  
+              int RequestId;         
+ */
 class Op {
 public:
     // Your definitions here.
@@ -128,7 +133,6 @@ public:
     std::string Value;
     std::string ClientId;  // 客户端号码
     int RequestId;         // 客户端号码请求的Request的序列号，为了保证线性一致性
-                    //  IfDuplicate bool // Duplicate command can't be applied twice , but only for PUT and APPEND
 
 public:
     // todo
@@ -150,7 +154,7 @@ public:
         boost::archive::text_iarchive ia(iss);
         // read class state from archive
         ia >> *this;
-        return true;  // todo : 解析失敗如何處理，要看一下boost庫了
+        return true;  // todo : 解析失败如何处理，要看一下boost库
     }
 
 public:
