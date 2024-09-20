f437d0ef83b9fe302889c8e45075439feebec341
SuycxZMZ <suycx65@163.com>
2024-09-20 12:08:19 +0800

    修改rpcprovider的成员命名

--- src/rpc/KVrpcprovider.cc
+++ src/rpc/KVrpcprovider.cc
@@ -40,23 +40,23 @@ void KVRpcProvider::KVRpcProviderRunInit(int nodeIndex, short port) {
 }
 
 void KVRpcProvider::InnerStart() {
-    m_isrunning = true;
+    m_isRunning = true;
     std::cout << " --------------- 绑定到子类 InnerStart() --------------- \n";
     auto addr = sylar::Address::LookupAny(m_ipPort);
     std::vector<sylar::Address::ptr> addrs;
     addrs.push_back(addr);
     std::vector<sylar::Address::ptr> fails;
-    while(!m_TcpServer->bind(addrs, fails)) {
+    while(!m_tcpServer->bind(addrs, fails)) {
         sleep(2);
     }
     std::cout << " --------------- bind success, " << m_ipPort << " at:" << getpid() << "---------------" << std::endl;
 
     // 开启 tcpserver
-    m_TcpServer->start();
+    m_tcpServer->start();
 }
 
 KVRpcProvider::~KVRpcProvider() {
     std::cout << "[func - RpcProvider::~RpcProvider()]: ip和port信息：" << m_ipPort << std::endl;
-    m_isrunning = false;
+    m_isRunning = false;
     m_iom->stop();
 }
