5c317c19e9d87a9aeabe171886fe6cc0717e7823
SuycxZMZ <suycx65@163.com>
2024-10-14 20:53:38 +0800

    优化编译脚本

--- build.sh
+++ build.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
+
+# 启用调试输出
 set -x
 
-sh configureproto.sh
+# 解析命令行参数
+BUILD_TYPE="DEBUG"
+BUILD_DIR="cmake-build-debug"
 
-# 检查是否有 cmake-build-debug 目录存在，如果没有，则创建该目录。如果存在，则清除其中的内容
-if [ ! -d "cmake-build-debug" ]; then
-    mkdir cmake-build-debug
-else
-    rm -rf cmake-build-debug/*
+if [[ "$1" == "RELEASE" ]]; then
+    BUILD_TYPE="RELEASE"
+    BUILD_DIR="cmake-build-release"
 fi
 
-# 进入 cmake-build-debug 目录
-cd cmake-build-debug
+# 调用配置 proto 文件的脚本
+sh configureproto.sh
 
-# 生成 Makefile
-cmake ..
+# 检查是否存在构建目录
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir "$BUILD_DIR"
+else
+    rm -rf "$BUILD_DIR/*"
+fi
+
+# 进入构建目录
+cd "$BUILD_DIR"
+
+# 根据构建类型生成 Makefile
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 
 # 编译项目
-make
+make -j2
--- configureproto.sh
+++ configureproto.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -x
 
-cd src/rpc
-protoc *.proto --cpp_out=./
-
-cd ../raftRpcPro
-protoc *.proto --cpp_out=./
+cd ./src/raftRpcPro
+protoc --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) raftRPC.proto
+protoc --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) kvServerRPC.proto
 
 cd ../..
-cd example/rpcExample
-protoc *.proto --cpp_out=./
+
 
