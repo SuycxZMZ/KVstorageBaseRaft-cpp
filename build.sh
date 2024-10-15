2c351cb2c94fe59f4022af6454d809718a0a7b5b
SuycxZMZ <suycx65@163.com>
2024-10-15 14:42:25 +0800

    修改一下编译脚本

--- build.sh
+++ build.sh
@@ -12,8 +12,29 @@ if [[ "$1" == "RELEASE" ]]; then
     BUILD_DIR="cmake-build-release"
 fi
 
+# 检查是否已经有 spdlog
+SPDLOG_DIR="thirdParty/spdlog"
+SPDLOG_VERSION="v1.14.0"
+
+if [ ! -d "$SPDLOG_DIR" ]; then
+    echo "spdlog not found. Cloning spdlog $SPDLOG_VERSION..."
+    git clone --branch $SPDLOG_VERSION --depth 1 https://github.com/gabime/spdlog.git "$SPDLOG_DIR"
+    
+    # 进入 spdlog 目录并编译
+    mkdir -p "$SPDLOG_DIR/build"
+    cd "$SPDLOG_DIR/build"
+    cmake ..
+    make -j2
+    cd -
+fi
+
+
 # 调用配置 proto 文件的脚本
-sh configureproto.sh
+cd ./src/raftRpcPro
+protoc --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) raftRPC.proto
+protoc --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) kvServerRPC.proto
+cd ../..
+
 
 # 检查是否存在构建目录
 if [ ! -d "$BUILD_DIR" ]; then
